7a538bea6857ff2d3bd4327bf9b6fae57de07a40
inanç telci <inanctelci1@gmail.com>
2021-11-19 16:05:26 +0300

    odevin son hali

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,104 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Cape Jason',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Ordu, TURKEY',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        '     Cape Jason Natural and Archeological Sit Area is on the borders of Çaytepe village in '
+        'Perşembe county, on a small peninsula facing the sea. This area is currently a '
+        'governmental environmental protection area, classified as second degree. '
+        'The church still stands here, with the ruins of its garden wall. Parts of these ruins '
+        ' can be found all over the coast of the sea as well. Ancient ports and fish breeding '
+        'pools can also still be seen today.',
+        softWrap: true,
+        style: TextStyle(fontWeight: FontWeight.w400),
+      ),
+    );
+
+    return MaterialApp(
+      title: 'flutter layout demo',
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Image.asset('images/jason.jpg',
+                width: 600, height: 240, fit: BoxFit.cover),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
